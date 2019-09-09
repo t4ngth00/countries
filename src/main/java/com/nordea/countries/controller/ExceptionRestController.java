a6b94c7b4de83579bd0f3dd5e1d604598d7fd667
Nhan Nguyen <nguyenthanhnhan9516@gmail.com>
2019-09-09 04:23:59 +0300

    Fixed issues that the exception response status code is 200.

--- src/main/java/com/nordea/countries/controller/ExceptionRestController.java
+++ src/main/java/com/nordea/countries/controller/ExceptionRestController.java
@@ -2,6 +2,7 @@ package com.nordea.countries.controller;
 
 import com.nordea.countries.dto.response.RestClientExceptionDTO;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.RestClientResponseException;
@@ -9,10 +10,10 @@ import org.springframework.web.client.RestClientResponseException;
 @RestControllerAdvice
 public class ExceptionRestController {
     @ExceptionHandler(RestClientResponseException.class)
-    public RestClientExceptionDTO handleEmployeeNotFoundException(RestClientResponseException e) {
+    public ResponseEntity<RestClientExceptionDTO> handleEmployeeNotFoundException(RestClientResponseException e) {
         int statusCode = e.getRawStatusCode();
         String statusText = HttpStatus.valueOf(statusCode).getReasonPhrase();
 
-        return new RestClientExceptionDTO(statusCode, statusText);
+        return ResponseEntity.status(statusCode).body(new RestClientExceptionDTO(statusCode, statusText));
     }
 }
