8378524595357e12700b73ebf8309257ac247768
Nhan Nguyen <nguyenthanhnhan9516@gmail.com>
2019-09-09 09:50:37 +0300

    Reduced lombok imports.

--- src/main/java/com/nordea/countries/dto/CountryDTO.java
+++ src/main/java/com/nordea/countries/dto/CountryDTO.java
@@ -1,9 +1,6 @@
 package com.nordea.countries.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
